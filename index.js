3b262319a12f8647aad184be8069871a6f873df7
Jan Dihlmann <m.dihlmann@googlemail.com>
2016-04-28 17:10:16 +0200

    Change

--- index.js
+++ index.js
@@ -1,86 +1,93 @@
-// Require Modules
-var express = require('express')
+// Express Module 
+var express = require('express');
+var app = express();
+
+// Other Modules
+var slackbot = require('slackbots');
+var server = require('http').createServer(app);  
 var jsonfile = require('jsonfile')
 var google = require('googleapis');
 var slackbot = require('slackbots');
-
-// Start Express
-var app = express();
-// Host Server
-app.set('port', (process.env.PORT || 5000));
-app.get('/', function(request, response) {
- 	response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));
-});
+var MongoClient = require('mongodb').MongoClient
+var format = require('util').format;
+ 
 
 
-// Setup Client API Key
-var api_key = process.env.GOOGLE_KEY;
+//Start Application And Socket Listen
+var port =  3000;
+server.listen(port);
+console.log("Server started on " + port);
 
-// Setup Youtube API
-var youtube = google.youtube('v3');
-
-//Setup File 
-var file = 'storage/storage.json'
-
-
-
-
-// Slackbot
-var bot = new slackbot({
-    token: process.env.SLACK_KEY,
-    name: 'casey'
+MongoClient.connect('mongodb://localhost/caseybot', function (err, db) {
+    if (err) {
+        throw err;
+    } else {
+        console.log("successfully connected to the database");
+    }
+    db.close();
 });
 
 
 
-// Send Youtube Requests
-function youtubeRequest() {
-	youtube.activities.list({key: api_key, part:'contentDetails', channelId: 'UCtinbF-Q-fVthA0qrFQTgXQ', maxResults: 1}, function(err, res) {
-			var content = res.items[0].contentDetails
-			if(content) {
-				var upload = content.upload
-				if(upload) {
-					var video = upload.videoId
-					var videoURL = 'https://www.youtube.com/watch?v=' + video
 
-					// Store Data 
-					jsonfile.readFile(file, function(err, res) {
-						if(!err) {
-							if(res.video != videoURL) {
-								writeData(videoURL)
-								bot.postMessageToChannel('general', videoURL, {as_user: true});
-							}
-						}
-					})
-				}
-			}
-	});
-}
+// // Setup Youtube API
+// var api_key = process.env.GOOGLE_KEY;
+// var youtube = google.youtube('v3');
+// //var currentURL = "getURL"
 
-bot.on('message', function(data) {
-	if(data.type == 'message' && data.channel == 'D0SDQ77BK' && data.user =='U0KDWE1DZ') {
-		bot.postMessageToUser('jan', 'ping'); 
-	}
-});
+// //Setup File 
+// var file = 'storage/storage.json'
 
 
 
-// Write Data to Storage 
-function writeData(videoURL) {
-	var obj = {video: videoURL}
-	jsonfile.writeFile(file, obj, function (err) {
-		if(err) {
-			console.log(err)
-		}
-	})
-}
+
+// // Slackbot
+// var bot = new slackbot({
+//     token: process.env.SLACK_KEY,
+//     name: 'casey'
+// });
 
 
-// Intervall
-var intervall = setInterval(function() { 
-	youtubeRequest()
-}, 30000);
+
+// // Send Youtube Requests
+// function youtubeRequest() {
+// 	youtube.activities.list({key: api_key, part:'contentDetails', channelId: 'UCtinbF-Q-fVthA0qrFQTgXQ', maxResults: 1}, function(err, res) {
+// 			var content = res.items[0].contentDetails
+// 			if(content) {
+// 				var upload = content.upload
+// 				if(upload) {
+// 					var video = upload.videoId
+// 					var videoURL = 'https://www.youtube.com/watch?v=' + video
+
+// 					// Store Data 
+// 					jsonfile.readFile(file, function(err, res) {
+// 						if(!err) {
+// 							if(res.video != videoURL) {
+// 								writeData(videoURL)
+// 								bot.postMessageToChannel('general', videoURL, {as_user: true});
+// 							}
+// 						}
+// 					})
+// 				}
+// 			}
+// 	});
+// }
+
+
+
+
+// // Write Data to Storage 
+// function writeData(videoURL) {
+// 	var obj = {video: videoURL}
+// 	jsonfile.writeFile(file, obj, function (err) {
+// 		if(err) {
+// 			console.log(err)
+// 		}
+// 	})
+// }
+
+
+// // Intervall
+// var intervall = setInterval(function() { 
+// 	youtubeRequest()
+// }, 30000);
