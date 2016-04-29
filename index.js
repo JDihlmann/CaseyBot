fd5941096fbf23b01a109a288a39478588bcf97e
Jan Dihlmann <m.dihlmann@googlemail.com>
2016-04-29 13:49:32 +0200

    Bot 1.0

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ var app = express();
 var slackbot = require('slackbots');
 var server = require('http').createServer(app);  
 var google = require('googleapis');
-var slackbot = require('slackbots');
 var mongoose = require('mongoose');
 var request = require('request');
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );    
+app.use(bodyParser.urlencoded({  
+  extended: true
+})); 
 
 
 //Start Application And Socket Listen
@@ -19,6 +23,7 @@ console.log("Server started on " + port);
 
 
 //MongoDB 
+//MAC 4321 // SERVER 27017
 mongoose.connect('mongodb://localhost:27017/caseybot');
 var db = mongoose.connection;
 
@@ -27,6 +32,7 @@ db.on('error', function (err) {
 });
 
 db.once('open', function () {
+	//test()
 	console.log('Database connected');
 });
 
@@ -47,19 +53,65 @@ var Video = mongoose.model('videos', videoSchema);
 //MongoDB Schema Companies
 var companies = db.collection('companies');
 var companySchema = mongoose.Schema({
-    token: String,
-    teamID: String,
-    teamName: String
+	teamID: String,
+	teamName: String, 
+	botID: String,
+	botToken: String,
+    globalToken: String
 });
 
+
+
+// Look for better request with JSON Parser
 companySchema.methods.sendToTeam = function (videoURL) {
-	var bot = new SlackBot({
-    	token: this.token,
-    	name: 'Casey'
+	var token = this.botToken
+	request.post({url:'https://slack.com/api/rtm.start', form: {token: this.botToken}}, function(err, httpResponse, body){
+		if(!err) {
+			var parsedBody = JSON.parse(body)
+			if(parsedBody.ok) {
+				var channels = parsedBody.channels
+				for( var i = 0; i < channels.length; i++) {
+					var channel = channels[i]
+					if(channel.is_member && !channel.is_archived ) {
+						sendToChannel(channel.id, token , videoURL)
+					}
+				}
+			}
+		}
+
+	})
+
+}
+
+
+var Company = mongoose.model('companies', companySchema);
+
+
+function test() {
+	Company.find({}, function(err, companies) {
+		if(!err) {
+			for( var i = 0; i < companies.length; i++) {
+				companies[0].sendToTeam("https://www.youtube.com/watch?v=Pg727Z7l3xI")
+			}
+		} 
 	});
 }
 
-var Company = mongoose.model('companies', companySchema);
+
+function sendToChannel(channel, token, videoURL) {
+	form = {
+		token: token,
+		channel: channel,
+		text: videoURL,
+		as_user: true
+	}
+
+	request.post({url:'https://slack.com/api/chat.postMessage', form: form }, function(err, httpResponse, body){
+		console.log()
+	})
+} 
+
+
 
 
 
@@ -92,13 +144,18 @@ function videoUploaded(videoID) {
 	//Mongo DB Object
 	var currentVideo = new Video({ id: videoID });
 
-	saveVideoToDB(currentVideo)
+	saveToDB(currentVideo)
 	sendVideoToEveryTeam(currentVideo.getURL())
 }
 
 function sendVideoToEveryTeam(videoURL) {
-
-
+	Company.find({}, function(err, companies) {
+		if(!err) {
+			for( var i = 0; i < companies.length; i++) {
+				companies[0].sendToTeam(videoURL)
+			}
+		} 
+	});
 }
 
 
@@ -117,7 +174,7 @@ function handleError(err) {
 
 
 // Intervall
-var intervallFrequenz = 3000
+var intervallFrequenz = 30000
 var intervall = setInterval(function() { 
 	videoYoutubeRequest()
 }, intervallFrequenz);
@@ -139,12 +196,47 @@ app.get('/', function (req, res) {
 //Website
 app.get('/oauth', function (req, res) {
 
-	console.log(res)
+	var code = req.query.code
 
-	// var form = {
+	var form = {
+		code: code,
+		client_id: "19474255650.38637281299",
+		client_secret: process.env.CLIENT_SECRET,
+		redirect_uri: "http://localhost:3000/oauth"
 
-	// }
-	// request.post({url:'http://service.com/upload', form: {key:'value'}}, function(err,httpResponse,body){ 
 
-	// })
+	}
+
+	request.post({url:'https://slack.com/api/oauth.access', form: form}, function(err, httpResponse, body){
+
+		if(!err) {
+			var parsedBody = JSON.parse(body)
+			if(parsedBody.ok) {
+
+
+				Company.find({ 'teamID': parsedBody.team_id}, function(err, companies) {
+					if(!err && companies.length == 0) {
+						var currentCompany = new Company({ 
+							teamID: parsedBody.team_id,
+							teamName: parsedBody.team_name, 
+							botID: parsedBody.bot.bot_user_id, 
+							botToken: parsedBody.bot.bot_access_token,
+    						globalToken: parsedBody.access_token
+						});
+						saveToDB(currentCompany)
+					} 
+				})
+
+				res.send('<html><body><h3> Worked :) </h3></body></html>')
+			} else {
+				res.send('<html><body><h3> Error:' +  body + '</h3></body></html>')
+			}	
+		} else {
+			res.send('<html><body><h3> Error </h3></body></html>')
+		}
+
+	})
 });
+
+
+
