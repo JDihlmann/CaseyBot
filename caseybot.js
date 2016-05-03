f46f05fb357eeb8a410a8d14e2d2bb00481469f8
Jan Dihlmann <m.dihlmann@googlemail.com>
2016-05-03 23:41:37 +0200

    Mongositis Virus is active --> Doctor apointment tomorrow

--- caseybot.js
+++ caseybot.js
@@ -139,7 +139,7 @@ app.get('/oauth', function (req, res) {
 // /* WEBSITE ---> MOVE */
 app.get('/', function (req, res) {
 	var testString = '<a href="https://slack.com/oauth/authorize?scope=bot&client_id=19474255650.38637281299"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'
-	mongooses.Company.find({}, function(err, companies) {
+	mongoose.Company.find({}, function(err, companies) {
 		if(!err) {
 			res.send('<html><body><h3>' + testString + '</body></html>');
 		} else {
